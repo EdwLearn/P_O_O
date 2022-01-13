db1a6c569f4b9599e7fa0e506de5a93521b74c22
EdwLearn <edwardgiraldo101@gmail.com>
2022-01-13 14:21:45 -0500

    Aplicando herencia Java y PHP

--- PHP/Car.php
+++ PHP/Car.php
@@ -1,7 +1,16 @@
 <?php
 class Car {
-public $id = integer;
-public $license = string;
-public $driver = string;
-public $passenger = integer;
+    public $id = integer;
+    public $license = string;
+    public $driver = string;
+    public $passenger = integer;
+
+    public function __construct($license, $driver){
+        $this->license = $license;
+        $this->driver = $driver;
+    }
+    public function PrintDataCar(){
+        echo "license: $this->license Driver: ".$this->driver->name;
+    }
 }
+?>
